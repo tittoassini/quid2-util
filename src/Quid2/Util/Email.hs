c896b72b5aa30f2d1fa8fe91e18205144682581d
Pasqualino Titto Assini <tittoassini@gmail.com>
2014-05-10 18:24:02 +0100

    support for reading propellor private data

--- src/Quid2/Util/Email.hs
+++ src/Quid2/Util/Email.hs
@@ -1,20 +1,29 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings ,ScopedTypeVariables #-}
 module Quid2.Util.Email(email,titto,quidagent) where
 
 import Network.Mail.Client.Gmail
 import Network.Mail.Mime
 import Data.String
--- import qualified Data.ByteString
 import Control.Monad
-import Data.Text
+import Data.Text hiding (words,unwords)
+import Quid2.Util.PrivData
 
 t = email titto "prova" "da Quid2.Util.Email"
+
+y = getPwd
 
 titto = "tittoassini@gmail.com"
 
 quidagent = "quidagent@gmail.com"
 
-qpwd = "agent2011"
-
 email :: String -> String -> String -> IO ()
-email to title body = sendGmail (fromString quidagent) qpwd (Address Nothing (fromString quidagent)) [Address Nothing (fromString to)] [] [] (fromString title) (fromString body) []
+email to title body = do
+  qpwd <- getPwd  
+  sendGmail (fromString quidagent) (fromString qpwd) (Address Nothing (fromString quidagent)) [Address Nothing (fromString to)] [] [] (fromString title) (fromString body) []
+
+getPwd = do
+  mpwd <- getPrivData (Password quidagent)
+  case mpwd of
+    Nothing -> error $ unwords ["Cannot access pwd for",quidagent]
+    Just qpwd -> return qpwd
+
---
+++ src/Quid2/Util/PrivData.hs
@@ -0,0 +1,25 @@
+{-
+Access private data spun by propellor.
+-}
+module Quid2.Util.PrivData(getPrivData,PrivDataField(..)) where
+
+import Control.Applicative
+import System.Directory
+import qualified Data.Map as M
+import Propellor.Types hiding (Address)
+import Quid2.Util.Strict
+
+t = getPrivData (Password "quidagent@gmail.com")
+
+getPrivData :: PrivDataField -> IO (Maybe String)
+getPrivData field = do
+	Right (Just m) <- (readish <$>) <$> (strictTry $ readFile "/usr/local/propellor/privdata/local")
+	-- print m
+        return $ M.lookup field m
+
+
+-- Copied from Propellor:Utility.PartialPrelude 
+readish :: Read a => String -> Maybe a
+readish s = case reads s of
+	((x,_):_) -> Just x
+	_ -> Nothing
